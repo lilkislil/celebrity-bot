4e17e4be20106b75e35c67cde7579b6cf98019c6
lilkisllil <mashankissl@gmail.com>
2026-01-16 18:47:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,14 +46,14 @@ router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("Привет, меня зовут Ягами Лайт)")
+    await message.answer("Привет, меня зовут Ягами Лайт")
 
 @router.message()
 async def handle_message(message: types.Message):
     user_message = message.text
     try:
         response = client.chat.completions.create(
-            model="gemma2-9b-it",
+            model="llama-3.3-70b-versatile",
             messages=[
                 {"role": "system", "content": PERSONA},
                 {"role": "user", "content": user_message}
